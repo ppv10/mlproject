30a93716e57390742f65de3a65f386fd82764816
Pavel <koripave@gmail.com>
2026-01-20 17:05:00 +0200

    Model trainer and debugging

--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,8 @@ import sys
 import numpy as np
 import pandas as pd
 import dill
+from sklearn.metrics import r2_score
+#from sklearn.model_selection import RandomizedSearchCV
 
 from src.exception import CustomException
 
@@ -15,6 +17,37 @@ def save_object(file_path, obj):
 
         with open(file_path, "wb") as file_obj:
             dill.dump(obj, file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def evaluate_models(X_train, y_train,X_test,y_test,models):
+    try:
+        report = {}
+
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            #param=param[list(models.keys())[i]]
+
+            #rs = RandomizedSearchCV(model,para,cv=10)
+            #rs.fit(X_train,y_train)
+
+            #model.set_params(**rs.best_params_)
+            model.fit(X_train,y_train)
+
+            #model.fit(X_train, y_train)  # Train model
+
+            y_train_pred = model.predict(X_train)
+
+            y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+
+        return report
 
     except Exception as e:
         raise CustomException(e, sys)
