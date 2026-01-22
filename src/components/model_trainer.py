81034ac77b0d2ef1280b771da8600617b5e4e5c1
Pavel <koripave@gmail.com>
2026-01-22 15:06:57 +0200

    Hyperparameter training

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -7,7 +7,6 @@ from sklearn.ensemble import (AdaBoostRegressor, GradientBoostingRegressor, Rand
 from sklearn.metrics import r2_score
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.tree import DecisionTreeRegressor
-#from xgboost import XGBRegressor
 from sklearn.svm import SVR
 
 from src.exception import CustomException
@@ -37,13 +36,48 @@ class ModelTrainer:
                 "Decision Tree": DecisionTreeRegressor(),
                 "Gradient Boosting": GradientBoostingRegressor(),
                 "Linear Regression": LinearRegression(),
-                #"XGBRegressor": XGBRegressor(),
-                #"CatBoosting Regressor": CatBoostRegressor(verbose=False),
                 "AdaBoost Regressor": AdaBoostRegressor(),
                 "Support Vector Regressor": SVR(),
+                "K-Neighbors Regressor": KNeighborsRegressor(),
             }
 
-            model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,models=models)
+            params={
+                "Decision Tree": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'splitter':['best','random'],
+                    # 'max_features':['sqrt','log2'],
+                },
+                "Random Forest":{
+                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'max_features':['sqrt','log2',None],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Gradient Boosting":{
+                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    # 'criterion':['squared_error', 'friedman_mse'],
+                    # 'max_features':['auto','sqrt','log2'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Linear Regression":{},
+
+                "AdaBoost Regressor":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    # 'loss':['linear','square','exponential'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Support Vector Regressor":{},
+
+                "K-Neighbors Regressor":{
+                    'n_neighbors': [1,3,5,7,9,11,13,15,17,19,21,23,25,27,29,31,33,35,37,39,41,43,45,47,49],
+                    'weights' : ['uniform','distance'],
+                    'metric' : ['minkowski','euclidean','manhattan']
+                }
+                
+            }
+
+            model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,models=models,param=params)
 
             ## To get best model score from dict
             best_model_score = max(sorted(model_report.values()))
--- src/utils.py
+++ src/utils.py
@@ -5,7 +5,7 @@ import numpy as np
 import pandas as pd
 import dill
 from sklearn.metrics import r2_score
-#from sklearn.model_selection import RandomizedSearchCV
+from sklearn.model_selection import RandomizedSearchCV
 
 from src.exception import CustomException
 
@@ -21,18 +21,18 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
     
-def evaluate_models(X_train, y_train,X_test,y_test,models):
+def evaluate_models(X_train, y_train,X_test,y_test,models,param):
     try:
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
-            #param=param[list(models.keys())[i]]
+            para=param[list(models.keys())[i]]
 
-            #rs = RandomizedSearchCV(model,para,cv=10)
-            #rs.fit(X_train,y_train)
+            rs = RandomizedSearchCV(model,para,cv=10,n_jobs=-1)
+            rs.fit(X_train,y_train)
 
-            #model.set_params(**rs.best_params_)
+            model.set_params(**rs.best_params_)
             model.fit(X_train,y_train)
 
             #model.fit(X_train, y_train)  # Train model
